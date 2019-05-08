85e94881e12a2ed86260da8ee8d2940f08d801ae
umeshror <sarukumesh@gmail.com>
2019-05-08 16:54:00 +0530

    project added

--- index.js
+++ index.js
@@ -4,20 +4,21 @@ const process = require('process');
 
 const app = express();
 const client = redis.createClient({
+    // host will be the name of service mentioned in Docker-compose
     host: "redis-server",
     port: 6379
 });
 client.set('visits', 0);
 
 app.get('/', (req, res) => {
-  process.exit(0)
-  client.get('visits', (err, visits) => {
-    res.send('Number of visits is ' + visits);
-    client.set('visits', parseInt(visits) + 1);
-  });
+    process.exit(0)
+    client.get('visits', (err, visits) => {
+        res.send('Number of visits is ' + visits);
+        client.set('visits', parseInt(visits) + 1);
+    });
 });
 
 app.listen(8081, () => {
-  console.log('Listening on port 8081');
+    console.log('Listening on port 8081');
 });
 
